0fd2961ec59de83dc23477ed6b04f6de5ec89f0d
Koki Shibata <kenergyintense@gmail.com>
2021-07-20 14:33:55 +0900

    add: robots

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
+    'gatsby-plugin-robots-txt',
     'gatsby-plugin-gatsby-cloud',
     // Add your Google Analytics ID to the .env file to enable
     // Otherwise, this plugin can be removed

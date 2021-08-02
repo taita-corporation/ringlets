bcf5237e0f8c261b9e2dd6e2f6e6ce99ea3135bb
Koki Shibata <kenergyintense@gmail.com>
2021-08-02 11:01:22 +0900

    fix: top

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import { Layout } from '../components/layout';
 import Button from '../components/button';
@@ -38,6 +38,7 @@ export const query = graphql`
     allDatoCmsSpecialGuest {
       nodes {
         name
+        instagram
         id
         thumbImage {
           gatsbyImageData
@@ -53,14 +54,19 @@ export default function IndexPage({ data }) {
       <GatsbyImage image={data.datoCmsTopPage.heroImage.gatsbyImageData} />
       <div className={s.wrapper}>
         <div
+          /* eslint-disable-next-line react/no-danger */
           dangerouslySetInnerHTML={{
             __html: data.datoCmsTopPage.introductionNode.childMarkdownRemark.html,
           }}
           className={s.introduction}
         />
         <section>
-          <GatsbyImage image={data.datoCmsTopPage.closetImage.gatsbyImageData} className={s.closetImage} />
+          <GatsbyImage
+            image={data.datoCmsTopPage.closetImage.gatsbyImageData}
+            className={s.closetImage}
+          />
           <div
+          /* eslint-disable-next-line react/no-danger */
             dangerouslySetInnerHTML={{
               __html: data.datoCmsTopPage.closetIntroNode.childMarkdownRemark.html,
             }}
@@ -76,12 +82,25 @@ export default function IndexPage({ data }) {
             <h2>Special Guests</h2>
             <div>スペシャルゲスト</div>
           </div>
-          {data.allDatoCmsSpecialGuest.nodes.map(({ thumbImage, id }) => (
-            <GatsbyImage id={id} image={thumbImage.gatsbyImageData} />
+          {data.allDatoCmsSpecialGuest.nodes.map(({
+            thumbImage, id, instagram, name,
+          }) => (
+            <div key={id} className="grid grid-cols-2">
+              <Link to={`/special-guests/${instagram}`}>
+                <GatsbyImage image={thumbImage.gatsbyImageData} className="rounded-md" />
+                <div className="font-semibold text-xl">{name}</div>
+              </Link>
+            </div>
           ))}
+          <Button to="/special-guests">View More</Button>
         </section>
         <section>
+          <div className={s.heading}>
+            <h2>Featured</h2>
+            <div>フィーチャー</div>
+          </div>
           <ProductListing products={data?.shopifyCollection?.products} />
+          <Button to="/products">View More</Button>
         </section>
       </div>
     </Layout>
